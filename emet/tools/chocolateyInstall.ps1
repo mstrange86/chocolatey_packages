da8a8784115f6ab2c56bddf4268502fd7017ff78
Matt Strange <mstrange86@gmail.com>
2014-10-26 15:24:48 -0700

    emet 5.0

---
+++ emet/tools/chocolateyInstall.ps1
@@ -0,0 +1,10 @@
+﻿$packageName = 'emet'
+$installerType = 'MSI' 
+$url = 'http://download.microsoft.com/download/A/A/8/AA853FAE-7608-462E-B166-45B0F065BA13/EMET%20Setup.msi' # download url
+$silentArgs = '/quiet' 
+$validExitCodes = @(0) 
+
+# installer, will assert administrative rights
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
+
+
